8eaa1b2661cabaeceb6934118a1376c869d8cf0d
Manali Latkar <manalil@thoughtworks.com>
2014-04-17 18:58:09 +0530

    Revert "Adding host and deployment info to missing exists entries in the nova usage audit"
    This reverts commit 60be387b978cb32a8367589051ff5065ca10b79b.

--- reports/nova_usage_audit.py
+++ reports/nova_usage_audit.py
@@ -25,7 +25,6 @@ sys.path.append(os.environ.get('STACKTACH_INSTALL_DIR', '/stacktach'))
 
 import usage_audit
 
-from stacktach.models import InstanceUsage
 from stacktach import datetime_to_decimal as dt
 from stacktach import models
 from stacktach.reconciler import Reconciler
@@ -113,24 +112,14 @@ def _audit_launches_to_exists(launches, exists, beginning):
                         rec = reconciler.missing_exists_for_instance(*args)
                     msg = "Couldn't find exists for launch (%s, %s)"
                     msg = msg % (instance, expected['launched_at'])
-                    launched_at = dt.dt_from_decimal(expected['launched_at'])
-                    usage = InstanceUsage.find(instance, launched_at)[0]
-                    host = usage.host()
-                    deployment = usage.deployment()
-                    fails.append(['Launch', expected['id'], msg,
-                                  'Y' if rec else 'N', host, deployment])
+                    fails.append(['Launch', expected['id'], msg, 'Y' if rec else 'N'])
         else:
             rec = False
             if reconciler:
                 args = (launches[0]['id'], beginning)
                 rec = reconciler.missing_exists_for_instance(*args)
             msg = "No exists for instance (%s)" % instance
-            launched_at = dt.dt_from_decimal(launches[0]['launched_at'])
-            usage = InstanceUsage.find(instance, launched_at)[0]
-            host = usage.host()
-            deployment = usage.deployment()
-            fails.append(['Launch', '-', msg, 'Y' if rec else 'N', host,
-                          deployment])
+            fails.append(['Launch', '-', msg, 'Y' if rec else 'N'])
     return fails
 
 
@@ -240,7 +229,7 @@ def store_results(start, end, summary, details):
         'created': dt.dt_to_decimal(datetime.datetime.utcnow()),
         'period_start': start,
         'period_end': end,
-        'version': 7,
+        'version': 6,
         'name': 'nova usage audit'
     }
 
